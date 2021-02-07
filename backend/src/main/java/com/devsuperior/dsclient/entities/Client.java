cf3b0f451e94a4bc64c061862e194186f224b783
luis3977@gmail.com <luis3977@gmail.com>
2021-02-07 20:03:47 -0200

    Created DTO and added attributes client entity.

--- backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -35,7 +37,12 @@ public class Client implements Serializable {
 	@Column(name = "CHILDREN", precision = 2)
 	private Integer children;
 	
-	
+	@Column(name = "CREATED_AT", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+
+	@Column(name ="UPDATED_AT", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;	
+
 	public Client() {
 		
 	}
@@ -95,6 +102,24 @@ public class Client implements Serializable {
 
 	public void setChildren(Integer children) {
 		this.children = children;
+	}	
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
